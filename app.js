781166a8ec1a4f182b158191042328d687c4592d
Mohamedzahangir <mohamedzahangiralimolla@gmail.com>
2026-03-12 21:04:41 +0530

    added interactive shapes

--- app.js
+++ app.js
@@ -94,12 +94,12 @@ function toggleTheme() {
         body.classList.remove('light-mode');
         // themeToggle.innerHTML = '<span class="toggle-icon">&#9789;</span>'; // Removed: Handled by CSS
         localStorage.setItem('theme', 'dark');
-        if (favicon) favicon.href = 'assets/Website Assets/dark logo.png';
+        if (favicon) favicon.href = 'assets/Website%20Assets/dark%20logo.png';
     } else {
         body.classList.add('light-mode');
         // themeToggle.innerHTML = '<span class="toggle-icon">&#9728;</span>'; // Removed: Handled by CSS
         localStorage.setItem('theme', 'light');
-        if (favicon) favicon.href = 'assets/Website Assets/light logo.png';
+        if (favicon) favicon.href = 'assets/Website%20Assets/light%20logo.png';
     }
 }
 
@@ -111,9 +111,9 @@ function toggleTheme() {
         body.classList.add('dark-mode');
         body.classList.remove('light-mode');
         // themeToggle.innerHTML = '<span class="toggle-icon">&#9789;</span>'; // Removed: Handled by CSS
-        if (favicon) favicon.href = 'assets/Website Assets/dark logo.png';
+        if (favicon) favicon.href = 'assets/Website%20Assets/dark%20logo.png';
     } else {
-        if (favicon) favicon.href = 'assets/Website Assets/light logo.png';
+        if (favicon) favicon.href = 'assets/Website%20Assets/light%20logo.png';
     }
 })();
 
